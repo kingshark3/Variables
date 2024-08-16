bec11ecfe4bde196864dd6e82ce51196d17de0f9
Jayden Little <littljay000@warren.k12.in.us>
2024-08-16 10:17:41 -0400

    initail commit

--- shark1234.py
+++ shark1234.py
@@ -1,2 +1,7 @@
-print("num_apples= 20")
-print("num_oranges= 15")
+num_apple = 20
+num_orange = 15
+print("number_of_apples", num_apple)
+print("number_of_oranges", num_orange)
+num_orange = 0
+print("number_of_apples", num_apple)
+print("number_of_oranges", num_orange)
